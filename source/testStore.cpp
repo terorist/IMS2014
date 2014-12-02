b086920746e38d24ef28b399dd2cda43c665df08
akcinel <akcinel8@seznam.cz>
2014-12-02 12:31:25 +0100

    test

--- source/testStore.cpp
+++ source/testStore.cpp
@@ -1,6 +1,14 @@
-#include "libsim.h"
+#include "libsim/libsim.h"
+#include <iostream>
 
-int main() {
+int main() { 
   Store* store = new Store();
-  store->Enter();
+  store->Enter(2);
+  store->Enter(3);
+  store->Enter(8);
+  store->Enter(10);
+  store->Enter(1);
+  store->Enter(5);
+  store->Enter(7);
+  std::cout << store->queue[0].pop() << std::endl;
 }
