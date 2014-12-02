cfaa053815cdcf434272225ef1f17a83795d418c
Jiri Picek <picek@ebrana.cz>
2014-12-02 12:15:49 +0100

    store

--- source/libsim/store.h
+++ source/libsim/store.h
@@ -1,17 +1,33 @@
+#ifndef __STORE_H__
+#define __STORE_H__
+
+#include <map>
+#include <queue>
+#include "libsim.h"
+
 class Store {
 public:
   Store(int size = 1) {
     capacity = size;
-    seized = 0;
+    entered = 0;
   }
-  void seize(Process* proc, int priority = 0);
-  void release();
-  bool seized() {
+  void Enter(/*Process*/ int proc, int priority = 0) {
+    if (IsFull) {
+//      proc->Passivate();
+      queue[priority].push(proc); //TODO queue
+    }
+  }
+  void Leave();
   
+  bool IsFull() {
+    return !(capacity>entered);
   }
 protected:
   int capacity;
-  int seized;
+  int entered;
   //TODO
-  fronta queue;
+public:
+  std::map<std::queue</*Process*/int>> queue;
 };
+
+#endif // __STORE_H__
